5b81d061b5ff01b71eb37df28de278c0fe838267
zudikhin <uzudihin@gmail.com>
2022-09-12 00:07:01 +0300

    sidebar

--- app/js/main.js
+++ app/js/main.js
@@ -1,4 +1,14 @@
-$(document).ready(function () {
-    "use strict";
-    
+let sidebar = document.getElementById("aside");
+let sidebarBack = document.getElementById("sidebarBack");
+let sidebarLogo = document.getElementById("sidebarLogo");
+let main = document.getElementById("main");
+
+sidebarBack.addEventListener("click", function(){
+    sidebar.classList.add("hide");
+    main.classList.add("open");
+});
+
+sidebarLogo.addEventListener("click", function(){
+    sidebar.classList.remove("hide");
+    main.classList.remove("open");
 });
